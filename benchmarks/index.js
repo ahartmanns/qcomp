62f7543c37046a9cdfa3307f05d358ca72b0d98a
Arnd Hartmanns <a.hartmanns@utwente.nl>
2018-11-16 17:18:16 +0100

    Website/benchmarks: Added filtering by property type and included model descriptions in the full-text search in the model browser.

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -3,11 +3,13 @@ var qmcc = {};
 qmcc.params = [];
 qmcc.modelTypes = [ "CTMC", "DTMC", "MA", "MDP", "PTA" ];
 qmcc.originals = ko.observableArray();
+qmcc.propertyTypes = [ "P", "Pb", "E", "Eb", "Ei", "S" ];
 qmcc.models = ko.observableArray();
 qmcc.filter = {
 	name: ko.observable(""),
 	type: ko.observable(undefined),
 	original: ko.observable(undefined),
+	propertyType: ko.observable(undefined),
 	minStates: ko.observable(""),
 	maxStates: ko.observable("")
 };
@@ -15,16 +17,18 @@ qmcc.sortBy = ko.observable("");
 qmcc.sortAsc = ko.observable(true);
 qmcc.filteredModels = ko.computed(function()
 {
-	return this.models().filter(m =>
+	return qmcc.models().filter(m =>
 	{
 		var modelText = m.name.toUpperCase() + " "
 			+ m.short.toUpperCase() + " "
-			+ m.notes.toUpperCase();
-		return (modelText.includes(this.filter.name().toUpperCase())) // name (via .name and .short)
-		&& (this.filter.type() === undefined || m.type === this.filter.type().toLowerCase()) // model type
-		&& (this.filter.original() === undefined || m.original.split("-")[0] === this.filter.original()) // original formalism
-		&& (this.filter.minStates() === "" || isNaN(Number(this.filter.minStates())) || m.maxStates >= Number(this.filter.minStates())) // min. number of states
-		&& (this.filter.maxStates() === "" || isNaN(Number(this.filter.maxStates())) || m.minStates <= Number(this.filter.maxStates())) // max. number of states
+			+ m.notes.toUpperCase() + " "
+			+ m.description.toUpperCase();
+		return (modelText.includes(qmcc.filter.name().toUpperCase())) // name (via .name and .short)
+		&& (qmcc.filter.type() === undefined || m.type === qmcc.filter.type().toLowerCase()) // model type
+		&& (qmcc.filter.original() === undefined || m.original.split("-")[0] === qmcc.filter.original()) // original formalism
+		&& (qmcc.filter.propertyType() === undefined || m.properties.find(p => propertyTypeToShortString(p.type) === qmcc.filter.propertyType()) !== undefined) // property type
+		&& (qmcc.filter.minStates() === "" || isNaN(Number(qmcc.filter.minStates())) || m.maxStates >= Number(qmcc.filter.minStates())) // min. number of states
+		&& (qmcc.filter.maxStates() === "" || isNaN(Number(qmcc.filter.maxStates())) || m.minStates <= Number(qmcc.filter.maxStates())) // max. number of states
 	});
 }, qmcc);
 qmcc.selectedModel = ko.observable(null);
